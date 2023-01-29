1163b9cc142b44d8636889e8c5e6329f372b6380
Ramesh A <catchrameshhere@gmail.com>
2023-01-29 20:52:17 +0530

    added few files

---
+++ src/basic/Test.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class Test {
+	
+	public void m1() {
+		System.out.println("from m1");
+	}
+
+	public static void main(String[] args) {
+		Test obj = new Test();
+		obj.m1();
+
+	}
+
+}
